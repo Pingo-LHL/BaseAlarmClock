dedc1ad98837728395328fa243dce807d1678052
srohani <srohani@gmail.com>
2015-11-17 15:32:50 -0800

    Initial Commit

---
+++ AlarmClockProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlarmClockProject
+//
+//  Created by Samer Rohani on 2015-11-17.
+//  Copyright © 2015 Samer Rohani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
